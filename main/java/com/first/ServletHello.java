b5379075fa0c10f11002b9bc1ec0696a1337bda4
Unnimaya T <Unnimaya.T@godigit.com>
2024-08-16 21:21:11 +0530

    main

---
+++ main/java/com/first/ServletHello.java
@@ -0,0 +1,23 @@
+package com.first;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet (name="ServletHello" , urlPatterns = "/hello")
+public class ServletHello extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("hello world");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
